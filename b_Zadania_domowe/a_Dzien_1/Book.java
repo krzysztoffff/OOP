0d8e91bb827f8017a3d1a9c15d940336b65ee335
krzysztoffff <krzycho@pozaprawem.pl>
2018-02-01 22:55:43 +0100

    OOP exercising

--- b_Zadania_domowe/a_Dzien_1/Book.java
+++ b_Zadania_domowe/a_Dzien_1/Book.java
@@ -3,4 +3,22 @@ package b_Zadania_domowe.a_Dzien_1;
 
 public class Book {
 	
+	private int id;
+	private String title;
+	private boolean avaible = true;
+	private Author author;
+	private Author [] additionalAuthors;
+	
+	public Book (int id, String title) {
+		this.id = id;
+		this.title = title;
+		
+	}
+	
+	public Book (int id, String title, Author author) {
+		this.id = id;
+		this.title = title;
+		this.author = author;
+	}
+	
 }
