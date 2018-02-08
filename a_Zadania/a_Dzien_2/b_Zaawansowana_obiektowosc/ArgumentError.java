68071bdebc29a5d61b0ea5628c79127fc7bdf765
krzysztoffff <krzycho@pozaprawem.pl>
2018-02-08 20:06:42 +0100

    shopping cart

---
+++ a_Zadania/a_Dzien_2/b_Zaawansowana_obiektowosc/ArgumentError.java
@@ -0,0 +1,7 @@
+package a_Zadania.a_Dzien_2.b_Zaawansowana_obiektowosc;
+
+public class ArgumentError extends IllegalArgumentException{
+	
+//	TODO w³asny wyj¹tek ale nie wiem jeszcze czy bêdzie potrzebny
+
+}
--- a_Zadania/a_Dzien_2/b_Zaawansowana_obiektowosc/ShoppingCart.java
+++ a_Zadania/a_Dzien_2/b_Zaawansowana_obiektowosc/ShoppingCart.java
@@ -1,7 +1,24 @@
 package a_Zadania.a_Dzien_2.b_Zaawansowana_obiektowosc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jdk.nashorn.internal.runtime.ArgumentSetter;
 
 public class ShoppingCart {
+	
+	private List<Product> products = new ArrayList<>();
+	
+	
+//	public void addProduct(Product newProduct) throws ArgumentError{
+//		
+//		if (newProduct instanceof Product) 
+//			this.products.add(newProduct);
+//		else
+//			throws 
+//		
+//	}
+	
 	
 	
 }
