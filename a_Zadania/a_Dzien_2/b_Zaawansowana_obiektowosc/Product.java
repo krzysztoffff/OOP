0adccde85f5de5a8dc4019ce504001e315faa871
krzysztoffff <krzycho@pozaprawem.pl>
2018-02-08 18:53:33 +0100

    shopping cart

--- a_Zadania/a_Dzien_2/b_Zaawansowana_obiektowosc/Product.java
+++ a_Zadania/a_Dzien_2/b_Zaawansowana_obiektowosc/Product.java
@@ -3,30 +3,5 @@ package a_Zadania.a_Dzien_2.b_Zaawansowana_obiektowosc;
 
 public class Product {
 	
-private int id;
-private String name;
-private String discription;
-private double price;
-private int quantity;
-
-
-//konstruktor - powinien przyjmować opis, cenę i ilość produtku.
-public void getProduct(String discription, double price, int quantity){
-	
-	this.discription = discription;
-	this.price = price;
-	this.quantity = quantity;
-	
-}
-//atrybut id powiniem mieć możliwość wyłącznie odczytu.
-public void getProduct (int id){
-	this.id = id;
-	this.id++;
-	
-}
-
-
-
-	
 	
 }
--- a_Zadania/a_Dzien_2/b_Zaawansowana_obiektowosc/ShoppingCart.java
+++ a_Zadania/a_Dzien_2/b_Zaawansowana_obiektowosc/ShoppingCart.java
@@ -3,6 +3,5 @@ package a_Zadania.a_Dzien_2.b_Zaawansowana_obiektowosc;
 
 public class ShoppingCart {
 	
-	private Product [] products;
 	
 }
