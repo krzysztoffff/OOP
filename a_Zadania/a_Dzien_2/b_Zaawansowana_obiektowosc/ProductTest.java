b4947b3d51d1bbfdb947f104b83dc576058de17b
krzysztoffff <krzycho@pozaprawem.pl>
2018-02-08 18:58:26 +0100

    shopping cart

--- a_Zadania/a_Dzien_2/b_Zaawansowana_obiektowosc/ProductTest.java
+++ a_Zadania/a_Dzien_2/b_Zaawansowana_obiektowosc/ProductTest.java
@@ -3,15 +3,6 @@ package a_Zadania.a_Dzien_2.b_Zaawansowana_obiektowosc;
 public class ProductTest {
 	
 	
-	public static void main(String[] args) {
-		
-		Product prod = new Product();
-		
-		
-		
-		
-		
-		
-	}
+	
 
 }
