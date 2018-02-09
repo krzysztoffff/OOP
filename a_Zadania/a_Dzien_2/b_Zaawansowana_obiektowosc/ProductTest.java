420806b74ac1a16b5dbf370ea5b3548b3aa64d45
krzysztoffff <krzycho@pozaprawem.pl>
2018-02-09 12:00:41 +0100

    shopping cart

--- a_Zadania/a_Dzien_2/b_Zaawansowana_obiektowosc/ProductTest.java
+++ a_Zadania/a_Dzien_2/b_Zaawansowana_obiektowosc/ProductTest.java
@@ -3,8 +3,9 @@ package a_Zadania.a_Dzien_2.b_Zaawansowana_obiektowosc;
 public class ProductTest {
 
 	public static void main(String[] args) {
-
+		// tworzê obiekt
 		ShoppingCart sc = new ShoppingCart();
+		// dodajê produkty
 		sc.addProduct(new Product("Mas³o", "ekstra", 150, 2));
 		sc.addProduct(new Product("Ziemniak", "zwyk³y", 0.15, 8));
 		sc.addProduct(new Product("Cukierek", "s³odki", 2.15, 18));
@@ -12,7 +13,11 @@ public class ProductTest {
 		sc.addProduct(new Product("Ser", "bia³y", 13.84, 21));
 		sc.addProduct(new Product("Cukier", "bia³y", 2.84, 10));
 		sc.addProduct(new Product("Makaron", "sphagetti", 2.47, 2));
-
+		// zmieniam iloœæproduktu nr 7 czyli makaronu. By³o 2 na parakonie pojawi siê 3
+		sc.changeProductQuantity(7, 3);
+		// usuwam produkt o id 3 na parogonie nie pojawi siê
+		sc.removeProduct(3);
+		//drukujê paragon
 		sc.printReceipt();
 	}
 
