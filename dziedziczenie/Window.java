6ca3d60da54f46c3132bf32c4c13eeaadaff06c5
krzysztoffff <krzycho@pozaprawem.pl>
2018-02-12 22:12:21 +0100

    exercises

--- dziedziczenie/Window.java
+++ dziedziczenie/Window.java
@@ -14,13 +14,19 @@ public class Window extends Canvas{
 	
 	
 	
-	public Window (int width, int height, String title) {
+	public Window (int width, int height, String title, Game game) {
 		
 		JFrame frame = new JFrame(title);
 		frame.setPreferredSize(new Dimension(width, height));
 		frame.setMaximumSize(new Dimension(width, height));
 		frame.setMinimumSize(new Dimension(width, height));
 		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.add(game); //TODO
+		frame.setVisible(true);
+		game.start(); //TODO
 	}
 
 	
